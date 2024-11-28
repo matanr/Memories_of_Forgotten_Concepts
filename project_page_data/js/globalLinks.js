458c62e65e7e316e808e713070d83f88efd2cf90
Shimon Malnick <shimonmalnick@gmail.com>
2024-11-28 18:56:52 +0200

    using css instead of js for relsheets

--- project_page_data/js/globalLinks.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll('a[href]').forEach(link => {
-        link.setAttribute('target', '_blank');
-        link.setAttribute('rel', 'noopener noreferrer');
-    });
-});
